3d1fad816287d7e3b6778b4f113de5f3cc9d1caa
sergimol <sergimol@ucm.es>
2022-01-29 05:47:55 +0100

    Mi mando está roto :/

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    GameObject optionsFirstButton = null, optionsUI = null; // Referencian el menú de opciones y el botón seleccionado al abrirlo
+
+    private void Start() // Inicia la música del menú
+    {
+        //AudioManager.instance.Play(AudioManager.ESounds.Menu);
+    }
+
+    public void Play() // Para la música del menú y carga la primera escena
+    {
+        Cursor.visible = false;
+        SceneManager.LoadScene("Sergio");
+    }
+
+    public void Quit() // Cierra el juego
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        //SceneManager.LoadScene("Creditos");
+        Debug.Log("Aqui van los creditos");
+    }
+
+    public void Options() // Abre el menú de opciones
+    {
+        //optionsUI.SetActive(true);
+        //EventSystem.current.SetSelectedGameObject(null);
+        //EventSystem.current.SetSelectedGameObject(optionsFirstButton);
+        Debug.Log("Aqui van las opciones");
+    }
+
+    public void Deck()
+    {
+        Debug.Log("Aqui van las cartas");
+    }
+}
