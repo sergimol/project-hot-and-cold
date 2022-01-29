3b397609f7ded5b504d4b94baf479f9b5b49848c
sergimol <sergimol@ucm.es>
2022-01-29 01:55:37 +0100

    Checkeate tremendo glow

--- Assets/Scripts/Objects/Glow.cs
+++ Assets/Scripts/Objects/Glow.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class Glow : MonoBehaviour
 {
+    float sineAngle = 0;
+
     private void Update()
     {
         Debug.Log("danlles estoy brillando");
+        float sizeOffset = Mathf.Sin(sineAngle);
+        transform.localScale = new Vector3(1 + sizeOffset, 1 + sizeOffset, 1);
+        sineAngle += Time.deltaTime;
+    }
+
+    private void OnDisable()
+    {
+        //uwu
+        transform.localScale = new Vector3(1, 1, 1);
     }
 }
