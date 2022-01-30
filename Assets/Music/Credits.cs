560c58a52300e9803a63d924dedc57b803a3c688
Daniel Illanes Morillas <dailla01@ucm.es>
2022-01-30 05:14:53 +0100

    Musiquita

---
+++ Assets/Music/Credits.cs
@@ -0,0 +1,34 @@
+﻿// Monkeys Spinning Monkeys Kevin MacLeod (incompetech.com)
+// Licensed under Creative Commons: By Attribution 3.0 License
+// http://creativecommons.org/licenses/by/3.0/
+// Music promoted by https://www.chosic.com/free-music/all/ 
+
+// Fluffing a Duck Kevin MacLeod (incompetech.com)
+// Licensed under Creative Commons: By Attribution 3.0 License
+// http://creativecommons.org/licenses/by/3.0/
+// Music promoted by https://www.chosic.com/free-music/all/ 
+
+// Battle (Boss) by BoxCat Games | https://freemusicarchive.org/music/BoxCat_Games
+// Music promoted by https://www.chosic.com/free-music/all/
+// Creative Commons CC BY 3.0
+// https://creativecommons.org/licenses/by/3.0/
+ 
+
+//  Music: https://www.chosic.com/free-music/all/  ON FIRE
+
+// Circus Theme (Entry of the Gladiators) – Ragtime Version by Alexander Nakarada | https://www.serpentsoundstudios.com
+// Music promoted on https://www.chosic.com/free-music/all/
+// Creative Commons Attribution 4.0 International (CC BY 4.0)
+// https://creativecommons.org/licenses/by/4.0/
+ 
+
+//  Banjos, Unite! by Alexander Nakarada | https://www.serpentsoundstudios.com
+// Music promoted on https://www.chosic.com/free-music/all/
+// Creative Commons Attribution 4.0 International (CC BY 4.0)
+// https://creativecommons.org/licenses/by/4.0/
+ 
+
+//  Upbeat Forever Kevin MacLeod (incompetech.com)
+// Licensed under Creative Commons: By Attribution 3.0 License
+// http://creativecommons.org/licenses/by/3.0/
+// Music promoted by https://www.chosic.com/free-music/all/ 
